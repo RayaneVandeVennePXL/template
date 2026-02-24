837ef6cee5b41030a81ca12d1c98b74e6bb841bb
RayaneVandeVennePXL <148549542+RayaneVandeVennePXL@users.noreply.github.com>
2026-02-24 13:36:38 +0100

    Add hostname change and restart functionality

--- TemplateOne.ps1
+++ TemplateOne.ps1
@@ -1,1 +1,10 @@
 
+$newName = Read-Host "Vul nieuwe hostname in"
+
+Rename-Computer -NewName $newName -Force
+
+
+Write-Host "Hostname aangepast naar $newName. De server herstart over 2 seconden..." -ForegroundColor Cyan
+
+Start-Sleep -Seconds 2
+Restart-Computer
