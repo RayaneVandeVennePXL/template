2b47ca088c9e169eec5c6692e0765abf0e8e7f15
RayaneVandeVennePXL <148549542+RayaneVandeVennePXL@users.noreply.github.com>
2026-02-25 11:55:01 +0100

    Update TemplateOne.ps1

--- TemplateOne.ps1
+++ TemplateOne.ps1
@@ -3,11 +3,18 @@ $newName = Read-Host "Enter new hostname"
 Write-Host "Renaming to $newName..." -ForegroundColor Yellow
 Rename-Computer -NewName $newName -Force
 
-# 2. De trigger zetten voor het tweede script (na de reboot)
+# 2. Download Script 2 van GitHub
+if (!(Test-Path "C:\Scripts")) { New-Item -Path "C:\Scripts" -ItemType Directory -Force }
+
+# Gebruik de RAW link voor PowerShell downloads
+$script2Url = "https://raw.githubusercontent.com/RayaneVandeVennePXL/template/main/TemplateTwo.ps1"
+Invoke-WebRequest -Uri $script2Url -OutFile "C:\Scripts\TemplateTwo.ps1"
+
+# 3. Zet de RunOnce trigger voor de boot NA de herstart
 $runOncePath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
-$secondScript = "powershell.exe -ExecutionPolicy Bypass -File C:\Scripts\SiteConfig.ps1"
+$secondScript = "powershell.exe -ExecutionPolicy Bypass -File C:\Scripts\TemplateTwo.ps1"
 Set-ItemProperty -Path $runOncePath -Name "SiteConfigTask" -Value $secondScript
 
-Write-Host "Hostname set. Rebooting to apply changes..." -ForegroundColor Cyan
-Start-Sleep -Seconds 3
+Write-Host "Hostname set and Script 2 downloaded. Rebooting in 5s..." -ForegroundColor Cyan
+Start-Sleep -Seconds 5
 Restart-Computer
